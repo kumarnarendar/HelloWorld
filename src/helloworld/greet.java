736b8cfd11b79a6eb5e37da6e53ee9a883c9b87a
kumarnarendar <narendarramnani@yahoo.com>
2017-04-29 18:25:55 +0100

    solution

--- src/helloworld/greet.java
+++ src/helloworld/greet.java
@@ -9,14 +9,6 @@ package helloworld;
  *
  * @author Hashmi
  */
-public class HelloWorld {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-       System.out.println("hello");
-    }
+public class greet {
     
 }
